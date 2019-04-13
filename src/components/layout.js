b927e922680e9c761342fc21d8fa2bb40f2389b7
adamrasheed <adamrasheed91@gmail.com>
2019-04-12 22:28:40 -0700

    added perfect audience tracking code

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,50 +9,66 @@ import "../styles/main.scss"
 import Header from "./header"
 import FooterNav from "./Global/footerNav"
 
-const Layout = ({ children }) => (
-  <GlobalProvider>
-    <StaticQuery
-      query={graphql`
-        query SiteTitleQuery {
-          site {
-            siteMetadata {
-              title
-            }
-          }
-          file(relativePath: { eq: "royal-palm.png" }) {
-            childImageSharp {
-              fluid(maxHeight: 1440) {
-                ...GatsbyImageSharpFluid
+class Layout extends React.Component {
+  perfectAudience() {
+    window._pa = window._pa || {}
+    var pa = document.createElement("script")
+    pa.type = "text/javascript"
+    pa.async = true
+    pa.src =
+      ("https:" == document.location.protocol ? "https:" : "http:") +
+      "//tag.marinsm.com/serve/5cb16d62c10f65a1fc000003.js"
+    var s = document.getElementsByTagName("script")[0]
+    s.parentNode.insertBefore(pa, s)
+  }
+  render() {
+    const { children } = this.props
+    return (
+      <GlobalProvider>
+        <StaticQuery
+          query={graphql`
+            query SiteTitleQuery {
+              site {
+                siteMetadata {
+                  title
+                }
+              }
+              file(relativePath: { eq: "royal-palm.png" }) {
+                childImageSharp {
+                  fluid(maxHeight: 1440) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
               }
             }
-          }
-        }
-      `}
-      render={data => (
-        <>
-          <Header siteTitle={data.site.siteMetadata.title} />
-          <main>{children}</main>
-          <footer className="center small footer">
-            <div className="footer__credits">
-              © {new Date().getFullYear()},{" "}
-              <a
-                href="https://adamrasheed.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Adam Rasheed
-              </a>
-              {"    "}
-              <span>Not affiliated with Shopify®</span>
-            </div>
-
-            <FooterNav />
-          </footer>
-        </>
-      )}
-    />
-  </GlobalProvider>
-)
+          `}
+          render={data => (
+            <>
+              <Header siteTitle={data.site.siteMetadata.title} />
+              <main>{children}</main>
+              <footer className="center small footer">
+                <div className="footer__credits">
+                  © {new Date().getFullYear()},{" "}
+                  <a
+                    href="https://adamrasheed.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Adam Rasheed
+                  </a>
+                  {"    "}
+                  <span>Not affiliated with Shopify®</span>
+                </div>
+                {this.perfectAudience()}
+                <FooterNav />
+              </footer>
+            </>
+          )}
+        />
+      </GlobalProvider>
+    )
+  }
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
